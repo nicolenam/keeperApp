11acedfc5304e15bceb08e71775cf4f15ba84529
nicole-nam <eunjungnam@gmail.com>
2023-06-15 14:08:29 -0400

    Added helpful comments regarding object method

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,20 +9,25 @@ const Note = () =>{
 
     const [ notesArray, setNotesArray ] = useState([]);
 
+    // useEffect hook is a React hook that allows you to perform side effects in functional components. It runs after the component has rendered or when certain dependencies change
     useEffect(() => {
+
+      // 'getNotes' that fetches the notes from the database
         const getNotes = () => {
 
             onValue(notesRef, (snapshot) => {
-            
+
+              // function is used to listen for changes in the notesRef location in the database. Whenever there is a change, the provided callback function is executed
               const data = snapshot.val();
+              console.log(data)
 
               if(data) {
+                // maps over the keys of the data object using Object.keys(data). For each key, it creates a new note object with the key as the id property and spreads the remaining properties from data[key]
                 const newNotes = Object.keys(data).map((key) => ({
                   id: key, 
                   ...data[key],
                 }));
                 setNotesArray(newNotes);
-
               }else{
                 setNotesArray([])
               }
